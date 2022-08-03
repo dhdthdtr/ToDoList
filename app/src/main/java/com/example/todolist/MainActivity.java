ad52716bb7bbdbe1bcaecf51679fa5ce9d497eee
dhdthdtr <nguyenvubang011001hiie@gmail.com>
2022-08-04 03:53:43 +0700

    add animation on startup

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -6,15 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final int SPLASH = 3000;
+
+    Animation topAnim, bottomAnim;
+    ImageView imgView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom);
+        imgView = findViewById(R.id.imgView);
+        textView = findViewById(R.id.text);
+
+        imgView.setAnimation(topAnim);
+        textView.setAnimation(bottomAnim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
